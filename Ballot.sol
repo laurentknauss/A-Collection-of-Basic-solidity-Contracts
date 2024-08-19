b03fa0e1a7c4d712e9436ab92162b809e33a15b9
Laurent Knauss <laurentknauss@protonmail.com>
2024-08-19 13:47:09 +0200

    Update Ballot.sol

--- Ballot.sol
+++ Ballot.sol
@@ -1,41 +1,40 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.24;
 
 /** 
- * @title Ballot
- * @dev Implements voting process along with vote delegation
- */
+* Overall logic of this contract : 
+* -The constructor initializes the contract by setting the 'chairperson' to the address that deploys the contract and assigns him/her the right to vote;
+* -Proposals for votes are stored in an array .
+* Voting process : voters can cast their votes for a proposal and the counts of voted proposals are updated accordingly.
+* The winning proposal is selected based on the highest number of votes for it and the ame of the highest voted proposal is provided.
+
+
+
+/// @title Ballot
+/// @dev Implements a simple voting process .
 contract Ballot {
 
-    struct Voter {
-        uint weight; // weight is accumulated by delegation ( how many votes this person has) . If another person gives his vote to the voter,
-        // the voter weight  will go up. 
-
-        bool voted;  // if true, that person already voted, if not , it will be false. 
-
-        address delegate; // person delegated to ( if i give my vote to someo,ne else, i will send to this address)
-
-        uint vote;   // index of the voted proposal
+///@notice Custom error for when, a voter has already voted .
+error alreadyVoted(address voter);
     }
 
+    /// @notice Represents a single proposal
     struct Proposal {
-        // If you can limit the length to a certain number of bytes, 
-        // always use one of bytes1 to bytes32 because they are much cheaper
-        bytes32 name;   // short name (up to 32 bytes)
-        uint voteCount; // number of accumulated votes
+        bytes32 proposalName; 
+        /// @notice Number of accumulated votes 
+        uint voteCount; 
     }
 
+    /// @notice Address of the chair person who deployed the contract 
     address public chairperson;
 
     mapping(address => Voter) public voters;
 
     Proposal[] public proposals;
 
-    /** 
-     * @dev Create a new ballot to choose one of 'proposalNames'.
-     * @param proposalNames names of proposals
-     */
+    /// @
+     
     constructor(bytes32[] memory proposalNames) { // The array is of bytes32 type 
         chairperson = msg.sender; // we are setting  a "chairperson" variable to the value of msg.sender meanign whover is deploying the contract will be 
         //the sender 
@@ -43,9 +42,9 @@ contract Ballot {
         voters[chairperson].weight = 1; // A mapping  of voters 
 
 
-
+        /// Initializing proposals 
         for (uint i = 0; i < proposalNames.length; i++) {
-            // 'Proposal({...})' creates a temporary
+            /// @dev 'Proposal({...})' creates a temporary
             // Proposal object and 'proposals.push(...)'
             // appends it to the end of 'proposals'.
             proposals.push(Proposal({
@@ -55,58 +54,15 @@ contract Ballot {
         }
     }
 
-    /** 
-     * @dev Give 'voter' the right to vote on this ballot. May only be called by 'chairperson'.
-     * @param voter address of voter
-     */
-    function giveRightToVote(address voter) public {
-        require(
-            msg.sender == chairperson,
-            "Only chairperson can give right to vote."
-        );
-        require(
-            !voters[voter].voted,
-            "The voter already voted."
-        );
-        require(voters[voter].weight == 0);
-        voters[voter].weight = 1;
-    }
-
-    /**
-     * @dev Delegate your vote to the voter 'to'.
-     * @param to address to which vote is delegated
-     */
-    function delegate(address to) public {
-        Voter storage sender = voters[msg.sender];
-        require(!sender.voted, "You already voted.");
-        require(to != msg.sender, "Self-delegation is disallowed.");
-
-        while (voters[to].delegate != address(0)) {
-            to = voters[to].delegate;
-
-            // We found a loop in the delegation, not allowed.
-            require(to != msg.sender, "Found loop in delegation.");
-        }
 
 
-        sender.voted = true;
-        sender.delegate = to;
-        Voter storage delegate_ = voters[to];
-        if (delegate_.voted) {
-            // If the delegate already voted,
-            // directly add to the number of votes
-            proposals[delegate_.vote].voteCount += sender.weight;
-        } else {
-            // If the delegate did not vote yet,
-            // add to her weight.
-            delegate_.weight += sender.weight;
-        }
-    }
-
-
-
+     /// @dev Anyone can vote and call this 'vote' function 
+     /// @dev A caller of this function is automatically granted the right to vote if he/she has not voted yet.
     function vote(uint proposal) public {
         Voter storage sender = voters[msg.sender];
+
+        if (:sender.voted) { 
+
         require(sender.weight != 0, "Has no right to vote");
         require(!sender.voted, "Already voted.");
         sender.voted = true;
@@ -130,10 +86,8 @@ contract Ballot {
         }
     }
 
+    /// 
+    function winningProposalName() public view returns (bytes32 winnerName_) {
+            winnerName_ = proposals[winningProposal()].name; 
     
-    function winnerName() external view 
-            returns (bytes32 winnerName_)
-    {
-        winnerName_ = proposals[winningProposal()].name;
-    }
 }
